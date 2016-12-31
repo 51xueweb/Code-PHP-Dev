a907646da02b893ff44378941406e061e313b4e4
mico-cm <13014696853@163.com>
2016-12-31 15:18:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <body>
 		<!--top开始-->
 		<div class="top">
-			<div class="logo"><img src="images/logo.png"></div>
-			<div class="computer"><img src="images/computer.png"></div>
+			<div class="logo"><img src="./phpWebsite/images/logo.png"></div>
+			<div class="computer"><img src="./phpWebsite/images/computer.png"></div>
 		</div>
 		<!--top结束-->
 		<!--内容开始-->
@@ -36,13 +36,7 @@
 				<div class="list">
 					<?php
 						require("./phpWebsite/conn.php");
-						/**1.传入页码 **/
-						$page=@$_GET['p']?@$_GET['p']:1;//url中传入的值 (p=1/2/3...)
-						/**2.传入分页数据**/
-						$pre=($page-1)*10;
-						$pagesize=10;
-						$showpage=5;
-						$sql="SELECT * FROM `000` order by id LIMIT $pre,$pagesize";
+						$sql="SELECT * FROM `000` order by id ";
 						$stmt=$dbh->query($sql);
 						while(list($id,$name,$if_db,$if_web,$if_jq,$code_nums,$size,$show_addr,$view_addr)=$stmt->fetch(PDO::FETCH_NUM)){
 
@@ -61,66 +55,6 @@
 					</ul>
 					<?php
 						}
-						// 实现数据分页输出
-						$total_sql="SELECT * FROM `000`";
-						$query=$dbh->query($total_sql);
-						$total=$query->rowCount();  // 数据条数
-						//计算页数，ceil向上取1,floor向下取1 
-						$total_page=ceil($total/$pagesize );
-						/**显示数据，分页条**/
-						$page_banner="<div class='page' align='center' >";
-						//计算偏移量
-						$pageoffset=($showpage-1)/2;
-						if($page>1){
-							$page_banner.="<a href=".$_SERVER['PHP_SELF']."?p=1>首页</a>";
-							$page_banner.="<a href=".$_SERVER['PHP_SELF']."?p=".($page-1)."><上一页</a>";
-						}else{
-							$page_banner.="<span class='disable'>首页</span>";
-							$page_banner.="<span class='disable'><上一页</span>";
-						}
-						//初始化数据
-						$start=1;
-						$end=$total_page;
-						if($total_page>$showpage){
-							if($page>$pageoffset+1){
-								$page_banner.="...";
-							}
-							if($page>$pageoffset){
-								$start=$page-$pageoffset;
-								$end=$total_page>$page+$pageoffset?$page+$pageoffset:$total_page;
-							}
-							else{
-								$start=1;
-								$end=$total_page>$showpage?$showpage:$total_page;
-							}
-							if($page+$pageoffset>$total_page){
-								$start=$start-($page+$pageoffset-$end);
-							}
-						}
-						for($i=$start;$i<=$end;$i++){
-							if($page==$i){
-								$page_banner.="<span class='current'>$i</span>";
-							}else{
-							  $page_banner.="<a href=".$_SERVER['PHP_SELF']."?p=".$i.">{$i}</a>";
-							}
-
-						}
-						if($total_page>$showpage&&$total_page>$page+$pageoffset){
-							$page_banner.="...";
-						}
-						if($page<$total_page){
-							$page_banner.="<a href=".$_SERVER['PHP_SELF']."?p=".($page+1).">下一页></a>";
-							$page_banner.="<a href=".$_SERVER['PHP_SELF']."?p=$total_page>尾页</a>";
-						}else{
-							$page_banner.="<span class='disable'>下一页></span>";
-							$page_banner.="<span class='disable'>尾页</span>";
-						}
-						$page_banner.="共{$total_page}页,";
-						$page_banner.="<form action='index.php' method='get' id='form'>";
-						$page_banner.="到第<input type='text' size='2' name='p'>页&nbsp;&nbsp;";
-						$page_banner.="<input type='submit' value='确定' id='btn'>";
-						$page_banner.="</form></div>";
-						echo $page_banner;
 					?>
 				</div>
 			</div>
